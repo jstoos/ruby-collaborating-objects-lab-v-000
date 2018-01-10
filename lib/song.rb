4e063ee07ccff8e48336149af8f9a9850f12b53a
Jennifer Stoos <jstoos@bitlaw.com>
2018-01-10 16:02:09 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -25,7 +25,7 @@ class Song
   def self.create(song_name, artist_name)
     new_song = self.new(song_name)
     @@all_songs << new_song
-    new_song.artist=artist_name
+    artist.name = new_song.artist
     new_song
   end
 
